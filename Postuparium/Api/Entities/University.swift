adceab6d99e907a49725b011b5d14a064ad051d6
d.boldin <d.boldin@corp.mail.ru>
2020-12-29 03:10:06 +0300

    get all universities

--- Postuparium/Api/Entities/University.swift
+++ Postuparium/Api/Entities/University.swift
@@ -12,15 +12,23 @@ import UIKit
 struct University:Codable {
     var scores: Int = 0
     var numberOfPrograms: Int = 0 // programs_counter
-    var place: Int = 0 //qs
+    var place: String = "" //qs
     var rating: Double = 0.0
     var name: String = ""
     var image: String = "" // preview
     var description: String = ""
     var id: Int = 0
     var site: String = ""
-    var ed_programs: [EdProgram] = []
     var photos: [Photo] = []
     var lat: Double = 0.0
     var lon: Double = 0.0
+    
+    private enum CodingKeys: String, CodingKey {
+        case scores, rating, name, id, site, photos, lat, lon
+        case place = "qs"
+        case numberOfPrograms = "programs_counter"
+        case image = "preview"
+        case description = "about"
+    }
  }
+
